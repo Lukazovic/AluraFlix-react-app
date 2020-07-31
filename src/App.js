c538444b0df60915130ba4b907453cba401d210e
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-07-31 10:28:14 -0300

    Update App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
+import Menu from './components/Menu';
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
+
 function App() {
-  return <h1>LucasFlix</h1>;
+  return (
+    <div style={{ background: '#141414' }}>
+      <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={'O que é Front-End?'}
+      />
+
+      {dadosIniciais.categorias.map((category, index) => (
+        <Carousel
+          key={index}
+          ignoreFirstVideo={index === 0 ? true : false}
+          category={category}
+        />
+      ))}
+
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
