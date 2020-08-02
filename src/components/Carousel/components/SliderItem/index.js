9ff9bdc41332b0901b821cdb8762fbd12eb3dd8b
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-08-01 22:22:39 -0300

    Add Slider Item

---
+++ src/components/Carousel/components/SliderItem/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const SliderItem = styled.li`
+  margin-right: 16px;
+  img {
+    margin: 16px;
+    width: 298px;
+    height: 197px;
+    object-fit: cover;
+  }
+`;
