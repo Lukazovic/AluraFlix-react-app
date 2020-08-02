b40d7f4b010e50e96617bf4f9b98229d42c72783
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-08-02 17:19:33 -0300

    Update Carousel to fit db data

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import { VideoCardGroupContainer, Title, ExtraLink } from './styles';
-import VideoCard from './components/VideoCard';
-import Slider from './components/Slider';
-import { SliderItem } from './components/SliderItem/index';
+import { VideoCardGroupContainer, Title, ExtraLink } from "./styles";
+import VideoCard from "./components/VideoCard";
+import Slider from "./components/Slider";
+import { SliderItem } from "./components/SliderItem/index";
 
 function VideoCardGroup({ ignoreFirstVideo, category }) {
-  const categoryTitle = category.titulo;
-  const categoryColor = category.cor;
+  const categoryTitle = category.title;
+  const categoryColor = category.color;
   const categoryExtraLink = category.link_extra;
   const videos = category.videos;
   return (
     <VideoCardGroupContainer>
       {categoryTitle && (
         <>
-          <Title style={{ backgroundColor: categoryColor || 'red' }}>
+          <Title style={{ backgroundColor: categoryColor || "red" }}>
             {categoryTitle}
           </Title>
           {categoryExtraLink && (
@@ -31,9 +31,9 @@ function VideoCardGroup({ ignoreFirstVideo, category }) {
           }
 
           return (
-            <SliderItem key={video.titulo}>
+            <SliderItem key={video.title}>
               <VideoCard
-                videoTitle={video.titulo}
+                videoTitle={video.title}
                 videoURL={video.url}
                 categoryColor={categoryColor}
               />
