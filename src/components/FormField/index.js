5138b7bed94fe4b2f1f1d995ff39ad79a6f6d289
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-08-02 12:25:46 -0300

    Update FormFiled to follow eslintrc rules

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
 // import { Container } from './styles';
 
 function FormField({ label, type, name, value, onChange }) {
   return (
     <fieldset>
-      <label htmlFor={name}>{label}: </label>
-      {type !== 'textarea' ? (
+      <label htmlFor={`id_${name}`}>{label}:</label>
+      {type !== "textarea" ? (
         <input
           type={type}
-          id={name}
+          id={`id_${name}`}
           name={name}
           value={value}
           onChange={onChange}
@@ -17,7 +18,7 @@ function FormField({ label, type, name, value, onChange }) {
       ) : (
         <textarea
           name={name}
-          id={name}
+          id={`id_${name}`}
           cols="30"
           rows="3"
           onChange={onChange}
@@ -26,5 +27,17 @@ function FormField({ label, type, name, value, onChange }) {
     </fieldset>
   );
 }
+
+FormField.defautProps = {
+  type: "text",
+};
+
+FormField.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
 
 export default FormField;
