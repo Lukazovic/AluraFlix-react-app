e41ac55eb33432ca26ebe8a0b3104d587db66dc9
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-08-02 17:19:03 -0300

    Update Home to use db data

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
-import PageDefault from '../../components/PageDefault';
+import PageDefault from "../../components/PageDefault";
+import BannerMain from "../../components/BannerMain";
+import Carousel from "../../components/Carousel";
 
-import BannerMain from '../../components/BannerMain';
-import Carousel from '../../components/Carousel';
-
-import dadosIniciais from '../../data/dados_iniciais.json';
+import Api from "../../services/api";
+import dadosIniciais from "../../data/dados_iniciais.json";
 
 function Home() {
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadData = async () => {
+      setLoading(true);
+      const response = await Api.getCategoriesWithVideos();
+      setCategories(response);
+      setLoading(false);
+    };
+    loadData();
+  }, []);
+
+  if (loading) {
+    return (
+      <PageDefault>
+        <h1>Loading</h1>
+      </PageDefault>
+    );
+  }
+
   return (
     <PageDefault>
       <BannerMain
-        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-        url={dadosIniciais.categorias[0].videos[0].url}
+        videoTitle={categories[0].videos[1].title}
+        url={categories[0].videos[1].url}
         videoDescription={
-          'O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!'
+          "O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"
         }
       />
 
-      {dadosIniciais.categorias.map((category, index) => (
+      {categories.map((category, index) => (
         <Carousel
-          key={index}
+          key={category.id}
           ignoreFirstVideo={index === 0 ? true : false}
           category={category}
         />
