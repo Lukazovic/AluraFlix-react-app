a75a49fdee87251655ee2483b1e833674cfc5faf
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-08-03 11:28:14 -0300

    Update FormField

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,32 +1,53 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import { Container, Label, Input } from "./styles";
+import { Container, Label, Input } from './styles';
 
-function FormField({ label, type, name, value, onChange, required }) {
+function FormField({
+  label,
+  type,
+  name,
+  value,
+  onChange,
+  required,
+  suggestions,
+}) {
+  const hasSuggestions = suggestions.length > 0;
   return (
     <Container>
       <Label htmlFor={`id_${name}`}>
         <Input
-          as={type === "textarea" ? "textarea" : "input"}
+          as={type === 'textarea' ? 'textarea' : 'input'}
           type={type}
           id={`id_${name}`}
           name={name}
           value={value}
+          list={hasSuggestions ? `suggestionFor_id_${name}` : undefined}
+          autoComplete={hasSuggestions ? 'off' : 'on'}
           onChange={onChange}
           required={required}
         />
         <Label.Text>{label}:</Label.Text>
+        {hasSuggestions && (
+          <datalist id={`suggestionFor_id_${name}`}>
+            {suggestions.map((suggestion) => (
+              <option key={suggestion} value={suggestion}>
+                {suggestion}
+              </option>
+            ))}
+          </datalist>
+        )}
       </Label>
     </Container>
   );
 }
 
-FormField.defautProps = {
-  type: "text",
-  value: "",
+FormField.defaultProps = {
+  type: 'text',
+  value: '',
   onChange: () => {},
   required: false,
+  suggestions: [],
 };
 
 FormField.propTypes = {
@@ -36,6 +57,7 @@ FormField.propTypes = {
   value: PropTypes.string,
   onChange: PropTypes.func,
   required: PropTypes.bool,
+  suggestions: PropTypes.arrayOf(PropTypes.string),
 };
 
 export default FormField;
