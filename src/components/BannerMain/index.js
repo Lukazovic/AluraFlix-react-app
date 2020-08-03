ea3bea363dcf69d8c3c36810dd0a3facc26b860d
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-08-03 09:44:19 -0300

    Update BannerMain

--- src/components/BannerMain/index.js
+++ src/components/BannerMain/index.js
@@ -1,43 +1,38 @@
-import React from 'react';
-import VideoIframeResponsive from './components/VideoIframeResponsive';
-import { BannerMainContainer, ContentAreaContainer, WatchButton } from './styles';
+import React from "react";
+import VideoIframeResponsive from "./components/VideoIframeResponsive";
+import {
+  BannerMainContainer,
+  ContentAreaContainer,
+  WatchButton,
+} from "./styles";
 
 function getYouTubeId(youtubeURL) {
-  return youtubeURL
-    .replace(
-      /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/,
-      '$7',
-    );
+  return youtubeURL.replace(
+    /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/,
+    "$7"
+  );
 }
 
-export default function BannerMain({
-  videoTitle,
-  videoDescription,
-  url,
-}) {
-  const youTubeID = getYouTubeId(url);
+export default function BannerMain() {
+  const youTubeID = getYouTubeId("https://youtu.be/XJ3sr259SDI");
   const bgUrl = `https://img.youtube.com/vi/${youTubeID}/maxresdefault.jpg`;
 
   return (
     <BannerMainContainer backgroundImage={bgUrl}>
       <ContentAreaContainer>
         <ContentAreaContainer.Item>
-          <ContentAreaContainer.Title>
-            {videoTitle}
-          </ContentAreaContainer.Title>
+          <ContentAreaContainer.Title>Imersão React</ContentAreaContainer.Title>
 
           <ContentAreaContainer.Description>
-            {videoDescription}
+            Conheça um pouco mais da Imersão React, um projeto da Alura para
+            ensinar React de graça durante a pandemia do COVID-19. Este Web Site
+            foi criado durante esta Imersão React.
           </ContentAreaContainer.Description>
         </ContentAreaContainer.Item>
 
         <ContentAreaContainer.Item>
-          <VideoIframeResponsive
-            youtubeID={youTubeID}
-          />
-          <WatchButton>
-            Assistir
-          </WatchButton>
+          <VideoIframeResponsive youtubeID={youTubeID} />
+          <WatchButton>Assistir</WatchButton>
         </ContentAreaContainer.Item>
       </ContentAreaContainer>
     </BannerMainContainer>
